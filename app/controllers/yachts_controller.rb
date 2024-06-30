1191beaee95ad79198ec437555d1ab160b0267f7
Jules Pinto <pintojules@gmail.com>
2024-07-01 00:14:34 +0100

    created models, routes and controllers

---
+++ app/controllers/yachts_controller.rb
@@ -0,0 +1,29 @@
+class YachtsController < ApplicationController
+  def index
+    @yachts = Yacht.all
+  end
+
+  def show
+    @yacht = Yacht.find(params[:id])
+  end
+
+  def new
+    @yacht = Yacht.new
+  end
+
+  def create
+    @yacht = Yacht.new(yacht_params)
+    @yacht.user = current_user
+    if @yacht.save
+      redirect_to @yacht, notice: "Yacht was successfully created."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def yacht_params
+    params.require(:yacht).permit(:name, :description, :price, :location)
+  end
+end
--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -1,3 +1,8 @@
 class Yacht < ApplicationRecord
   belongs_to :user
+
+  has_many :bookings
+  has_many :users, through: :bookings
+
+  validates :name, :description, :price, :location, presence: true
 end
